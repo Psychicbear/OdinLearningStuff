ea0d2e6fb1ccf5adc3e83e3be898edd4e50a2b95
Riley <rjscho@live.com.au>
2021-11-18 12:35:46 +1000

    Added past website I worked on

---
+++ i-want-flowers-website/scripts/query.js
@@ -0,0 +1,4 @@
+function flowerQuery() {
+  document.getElementById("frm1").submit();
+  alert("Thank you for your inquiry, a member of our team will email a response to you as soon as possible!");
+}
